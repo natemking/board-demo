39a97b08d4bda6e33313501a0acc79d80c9b6294
Nate M King <natemking@gmail.com>
2026-01-08 16:04:41 -0500

    feat: bootstrapped init sidebar on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+    SidebarTrigger,
+} from 'shadcn/sidebar';
+
 export default function HomePage(): React.JSX.Element {
     return (
-        <h1>
-            Hi
-        </h1>
+        <SidebarProvider className='overflow-y-hidden'>
+            <Sidebar
+                className='overflow-hidden'
+                collapsible='icon'
+            >
+                <SidebarHeader className='flex-row'>
+                    <SidebarTrigger />
+                    <span className='text-xl text-nowrap'>NMK Jobs</span>
+                </SidebarHeader>
+                <SidebarContent>Lorem Ipsum</SidebarContent>
+                <SidebarFooter>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton>
+                                Click
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarFooter>
+            </Sidebar>
+            <main className='flex-1'>Lorem ipsum</main>
+        </SidebarProvider>
     );
 }
