c2faa689bcaba85c7280b965c6e448a0296ef73e
Nate M King <natemking@gmail.com>
2026-01-12 11:53:07 -0500

    feat: intital clerk inngest create user function created

---
+++ src/lib/services/inngest/functions/clerk.ts
@@ -0,0 +1,14 @@
+import { inngest } from 'services/inngest/client';
+
+export const clerkCreateUser = inngest.createFunction(
+    {
+        id: 'clerk/create-db-user',
+        name: 'Clerk - Create DB User',
+    },
+    {
+        event: 'clerk/user.created'
+    },
+    async({event, step}) => {
+        
+    }
+);
