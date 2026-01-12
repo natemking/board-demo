e434a29474c9f04615cdb41e8fb61245dc1ddf3a
Nate M King <natemking@gmail.com>
2026-01-12 14:22:02 -0500

    feat: created lib/db dir and user.ts for db functions. added an insertUser method. applied new method to the clerk createUser webhook. added new TS path

---
+++ src/lib/db/user.ts
@@ -0,0 +1,10 @@
+import { db } from 'drizzle/db';
+import { UserTable } from 'drizzle/schema';
+
+export async function insertUser(user: typeof UserTable.$inferInsert): Promise<void> {
+    // insert user but if conflict on userId (already exits), then do an UPSERT
+    await db.insert(UserTable).values(user).onConflictDoUpdate({
+        target: UserTable.id,
+        set: user,
+    });
+}
--- src/lib/services/inngest/functions/clerk.ts
+++ src/lib/services/inngest/functions/clerk.ts
@@ -1,8 +1,16 @@
-
+import { Webhook } from 'svix';
+import { NonRetriableError } from 'inngest';
+import { insertUser } from 'db/user';
 import { inngest } from 'services/inngest/client';
+import { env } from 'env/server';
+import type { ClerkWebhookData } from 'types';
 
-
-
+function verifyWebhook<T>({
+    raw,
+    headers,
+}: Pick<ClerkWebhookData<T>['data'], 'raw' | 'headers'>): unknown {
+    return new Webhook(env.CLERK_WEBHOOK_SECRET).verify(raw, headers);
+}
 
 export const clerkCreateUser = inngest.createFunction(
     {
@@ -13,6 +21,36 @@ export const clerkCreateUser = inngest.createFunction(
         event: 'clerk/user.created',
     },
     async ({ event, step }) => {
+        await step.run('verify-webhook', async (): Promise<void> => {
+            try {
+                await verifyWebhook(event.data);
+            } catch (err) {
+                throw new NonRetriableError('Invalid webhook');
+            }
+        });
 
+        const userId = await step.run('create-user', async (): Promise<string> => {
+            const userData = event.data.data;
+            const email = userData.email_addresses.find(
+                em => em.id === userData.primary_email_address_id
+            );
+
+            if (!email) {
+                throw new NonRetriableError('No primary email address found')
+            }
+
+            await insertUser({
+                id: userData.id,
+                name: `${userData.first_name ?? ''} ${userData.last_name ?? ''}`,
+                imageUrl: userData.image_url,
+                email: email.email_address,
+                createdAt: new Date(userData.created_at),
+                updatedAt: new Date(userData.updated_at)
+            })
+            
+            return userData.id
+        });
+
+        
     }
 );
