3a8afc9bde45f854df29ef9c3effc7efc463b45a
Nate M King <natemking@gmail.com>
2026-02-12 14:43:48 -0500

    feat: added suspense boundry to employer layout and added get org. if no org redirect

--- src/app/employer/layout.tsx
+++ src/app/employer/layout.tsx
@@ -1,16 +1,27 @@
-import Link from 'next/link';
+import { Suspense } from 'react';
 import type { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 import { ClipboardList, PlusIcon } from 'lucide-react';
-import {
-    SidebarGroup,
-    SidebarGroupAction,
-    SidebarGroupLabel,
-} from 'components/shadcn/sidebar';
+import { SidebarGroup, SidebarGroupAction, SidebarGroupLabel } from 'components/shadcn/sidebar';
 import { AppSidebar } from 'components/sidebar/AppSidebar';
 import { SidebarNavMenuGroup } from 'components/sidebar/SidebarNavMenuGroup';
 import { SidebarOrganizationButton } from 'components/sidebar/SidebarOrganizationButton';
+import { getCurrentOrganization } from 'lib/services/clerk/getCurrentAuth';
 
 export default function EmployerLayout({ children }: { children: ReactNode }): React.JSX.Element {
+    return (
+        <Suspense>
+            <LayoutSuspense>{children}</LayoutSuspense>
+        </Suspense>
+    );
+}
+
+async function LayoutSuspense({ children }: { children: ReactNode }): Promise<React.JSX.Element> {
+    const { orgId } = await getCurrentOrganization();
+
+    if (!orgId) return redirect('/organizations/select');
+
     return (
         <AppSidebar
             content={
@@ -34,7 +45,7 @@ export default function EmployerLayout({ children }: { children: ReactNode }): R
                                 href: '/',
                                 icon: <ClipboardList />,
                                 label: 'Job Board',
-                            }
+                            },
                         ]}
                     />
                 </>
