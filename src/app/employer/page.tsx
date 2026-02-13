206f1df2de99abbdefba5f3420d8b7eb61c05c8e
Nate M King <natemking@gmail.com>
2026-02-13 11:00:38 -0500

    feat: created suspended page for the employer page

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -1,3 +1,18 @@
+import { Suspense } from 'react';
+import { getCurrentOrganization } from 'lib/services/clerk/getCurrentAuth';
+
 export default function EmployerHomePage(): React.JSX.Element {
+    return (
+        <Suspense>
+            <SuspendedPage />
+        </Suspense>
+    )
+}
+
+async function SuspendedPage(): Promise<React.JSX.Element | null> {
+    const { orgId } = await getCurrentOrganization();
+
+    if (!orgId) return null
+
     return <h1>Goodbye Employer</h1>;
 }
