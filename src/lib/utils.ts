6abdb764b7d35600a31e4fa196a120d2f0b8cd82
Nate M King <natemking@gmail.com>
2026-02-13 15:24:13 -0500

    feat: created the fist form line for title and wage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from 'clsx';
+import type { WageInterval } from 'drizzle/schema';
 
 export function cn(...inputs: ClassValue[]): string {
     return twMerge(clsx(inputs));
+}
+
+export function formatWageInterval(interval: WageInterval): 'Hour' | 'Year' {
+    switch (interval) {
+        case 'hourly':
+            return 'Hour';
+        case 'yearly':
+            return 'Year';
+
+        default:
+            throw new Error(`Invalid wage interval: ${interval satisfies never}`);
+    }
 }
--- src/ui/components/JobListingForm.tsx
+++ src/ui/components/JobListingForm.tsx
@@ -1,9 +1,28 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form } from 'components/shadcn/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import type z from 'zod';
+import { wageIntervals } from 'drizzle/schema';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from 'components/shadcn/form';
+import { Input } from 'components/shadcn/input';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from 'components/shadcn/select';
 import { jobListingFormZSchema } from 'lib/zSchema';
+import { formatWageInterval } from 'lib/utils';
 
 export function JobListingForm(): React.JSX.Element {
     const form = useForm({
@@ -17,13 +36,93 @@ export function JobListingForm(): React.JSX.Element {
             title: '',
             type: 'full-time',
             wage: null,
-            wageInterval: 'yearly'
-        }
-    })
-    
+            wageInterval: 'yearly',
+        },
+    });
+
+    const onSubmit = (data: z.infer<typeof jobListingFormZSchema>) => {
+        console.log(data);
+    };
+
     return (
         <Form {...form}>
-            Enter
+            <form
+                className='@container space-y-6'
+                onSubmit={e => void form.handleSubmit(onSubmit)(e)}
+            >
+                <div className='grid grid-cols-1 items-start gap-x-4 gap-y-6 @md:grid-cols-2'>
+                    <FormField
+                        control={form.control}
+                        name='title'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Job Title</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name='wage'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Wage</FormLabel>
+                                <div className='flex'>
+                                    <FormControl>
+                                        <Input
+                                            className='rounded-r-none'
+                                            {...field}
+                                            onChange={e => {
+                                                const { valueAsNumber } = e.target;
+
+                                                field.onChange(
+                                                    isNaN(valueAsNumber) ? null : valueAsNumber
+                                                );
+                                            }}
+                                            type='number'
+                                            value={field.value ?? ''}
+                                        />
+                                    </FormControl>
+                                    <FormField
+                                        control={form.control}
+                                        name='wageInterval'
+                                        render={({ field: intervalField }) => (
+                                            <FormItem>
+                                                <Select
+                                                    onValueChange={intervalField.onChange}
+                                                    value={intervalField.value ?? ''}
+                                                >
+                                                    <FormControl>
+                                                        <SelectTrigger className='rounded-l-none capitalize'>
+                                                            / <SelectValue />
+                                                        </SelectTrigger>
+                                                    </FormControl>
+                                                    <SelectContent>
+                                                        {wageIntervals.map(interval => (
+                                                            <SelectItem
+                                                                className='capitalize'
+                                                                key={interval}
+                                                                value={interval}
+                                                            >
+                                                                {formatWageInterval(interval)}
+                                                            </SelectItem>
+                                                        ))}
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <FormDescription>Optional</FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </form>
         </Form>
     );
 }
