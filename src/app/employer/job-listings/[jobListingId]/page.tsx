e739c11aeafc33c3e3c1d742b6d8db3ea42aa78b
Nate M King <natemking@gmail.com>
2026-02-16 16:03:15 -0500

    feat: applied new action to page

--- src/app/employer/job-listings/[jobListingId]/page.tsx
+++ src/app/employer/job-listings/[jobListingId]/page.tsx
@@ -1,23 +1,24 @@
 import { Suspense } from 'react';
 import { getCurrentOrganization } from 'lib/services/clerk/getCurrentAuth';
 import type { JobListingPageProps } from 'types';
+import { getJobListingById } from 'lib/actions';
 
 export default function JobListingPage(props: JobListingPageProps): React.JSX.Element {
     return (
         <Suspense>
-            <SuspendedPage {...props}/>
+            <SuspendedPage {...props} />
         </Suspense>
     );
 }
 
-async function SuspendedPage({params}: JobListingPageProps): Promise<React.JSX.Element | null> {
+async function SuspendedPage({ params }: JobListingPageProps): Promise<React.JSX.Element | null> {
     const { orgId } = await getCurrentOrganization();
 
     if (!orgId) return null;
 
     const { jobListingId } = await params;
 
-    return (
-        <h1>{jobListingId}</h1>
-    )
+    const jobListing = await getJobListingById(jobListingId, orgId);
+
+    return <pre>{JSON.stringify(jobListing, null, 2)}</pre>;
 }
