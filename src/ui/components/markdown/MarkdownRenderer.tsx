5aad6ff134c6cfba5ac8123845037ef127387acf
Nate M King <natemking@gmail.com>
2026-02-17 10:32:35 -0500

    feat: setup MarkdownRenderer to use remark plugins and take in extra options if need be

--- src/ui/components/markdown/MarkdownRenderer.tsx
+++ src/ui/components/markdown/MarkdownRenderer.tsx
@@ -1,9 +1,27 @@
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import remarkGfm from 'remark-gfm';
+import { cn } from 'lib/utils';
 import type { MarkdownRendererProps } from 'types';
 
-export function MarkdownRenderer({className, options, ...props}: MarkdownRendererProps): React.JSX.Element {
+export function MarkdownRenderer({
+    className,
+    options,
+    ...props
+}: MarkdownRendererProps): React.JSX.Element {
     return (
-        <div>
-            Enter
+        <div className={cn('markdown-prose', className)}>
+            <MDXRemote
+                {...props}
+                options={{
+                    mdxOptions: {
+                        remarkPlugins: [
+                            remarkGfm,
+                            ...options?.mdxOptions?.remarkPlugins ?? []
+                        ],
+                    },
+                    ...options?.mdxOptions
+                }}
+            />
         </div>
     );
 }
