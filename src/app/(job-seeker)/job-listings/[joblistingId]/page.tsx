bb6fa0c2c1be216541ebada763293b0264f67703
Nate M King <natemking@gmail.com>
2026-03-02 12:19:59 -0500

    feat: started building out the resizable feature on the JobLisitingPage

--- src/app/(job-seeker)/job-listings/[joblistingId]/page.tsx
+++ src/app/(job-seeker)/job-listings/[joblistingId]/page.tsx
@@ -1,8 +1,27 @@
+import { ResizablePanel, ResizablePanelGroup } from 'components/shadcn/resizable';
 import type { JobListingPageWithSearchParams } from 'types';
+import { JobListingItems } from 'components/job-listing/JobListingItems';
 
 export default function JobListingPage({
     params,
     searchParams,
 }: JobListingPageWithSearchParams): React.JSX.Element {
-    return null;
+    // const { defaultLayout, onLayoutChanged } = useDefaultLayout({
+    //     id: 'job-board-panel',
+    //     storage: localStorage,
+    // });
+
+    return (
+        <ResizablePanelGroup
+            // defaultLayout={defaultLayout}
+            // onLayoutChange={onLayoutChanged}
+            orientation='horizontal'
+        >
+            <ResizablePanel defaultSize={60} id='left' minSize={30}>
+                <div className='p-4 h-screen overflow-y-auto'>
+                    <JobListingItems params={params} searchParams={searchParams}/>
+                </div>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    );
 }
