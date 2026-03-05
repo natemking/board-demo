1776d32481882d5e2af235f3595b7310b6b48a83
Nate M King <natemking@gmail.com>
2026-03-05 16:26:56 -0500

    feat: created user-setting/resume dir and page. updated @sidebar parallel route

---
+++ src/app/(job-seeker)/@sidebar/user-settings/resume/page.tsx
@@ -0,0 +1,5 @@
+import { JobBoardSidebar } from 'components/sidebar/JobBoardSidebar';
+
+export default function JobBoardSidebarPage(): React.JSX.Element {
+    return <JobBoardSidebar />;
+}
---
+++ src/app/(job-seeker)/user-settings/resume/page.tsx
@@ -0,0 +1,7 @@
+export default function UserResumePage(): React.JSX.Element {
+    return (
+        <div className='max-w-3xl mx-auto py-8 space-y-6'>
+            <h1 className='text-2xl font-bold'>Upload Your Resume</h1>
+        </div>
+    );
+}
