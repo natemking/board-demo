f95ff9b3156b49ecc11b323be914553648dfc23b
Nate M King <natemking@gmail.com>
2026-03-10 10:42:45 -0400

    chore: removed env var

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -12,7 +12,6 @@ export const env = createEnv({
         CLERK_WEBHOOK_SECRET: z.string().min(1),
         UPLOADTHING_TOKEN: z.string().min(1),
         ANTHROPIC_API_KEY: z.string().min(1),
-        GEMINI_API_KEY: z.string().min(1)
     },
     createFinalSchema: envVars => {
         return z.object(envVars).transform(val => {
